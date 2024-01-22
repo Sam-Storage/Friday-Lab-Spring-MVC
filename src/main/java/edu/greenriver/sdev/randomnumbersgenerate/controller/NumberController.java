32b499886ef5deb1f2909146a024f4ea265b991a
samgrigorenko <samiam41028@gmail.com>
2024-01-21 20:56:29 -0800

    Commit

--- src/main/java/edu/greenriver/sdev/randomnumbersgenerate/controller/NumberController.java
+++ src/main/java/edu/greenriver/sdev/randomnumbersgenerate/controller/NumberController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class NumberController {
 
@@ -14,10 +17,42 @@ public class NumberController {
         this.service = service;
     }
 
+//    @RequestMapping("/random")
+//    public String genNumber(Model modelVars){
+//        modelVars.addAttribute("num", service.between(1,20));
+//        return "genNumber";
+//    }
+
+//    @RequestMapping("/random")
+//    public String genNumber(Model modelVars){
+//
+//        List<Integer> powerballNums = new ArrayList<>();
+//
+//        for (int i = 0; i < 5; i++){
+//            powerballNums.add(service.between(1,69));
+//        }
+//
+//        powerballNums.add(service.between(1,29));
+//        modelVars.addAttribute("powerballNums", powerballNums);
+//
+//        return "genNumber";
+//    }
+
     @RequestMapping("/random")
     public String genNumber(Model modelVars){
-        modelVars.addAttribute("num", service.between(1,20));
+
+        List<Integer> LargGroupNums = new ArrayList<>();
+
+        for (int i = 0; i < 50; i++){
+            LargGroupNums.add(service.between(1,20));
+        }
+
+        modelVars.addAttribute("LargGroupNums", LargGroupNums);
+
         return "genNumber";
     }
+
+
+
 
 }
